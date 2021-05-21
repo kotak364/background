1c4c6e8c43480a77d38d9bbc712e21f9b76bf3d8
kotak364 <75615832+kotak364@users.noreply.github.com>
2021-05-20 18:57:46 -0800

    add

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-const color = ["red","green","blue","yellow"];
+// js variable
+
+const color = ["red", "green", "blue", "yellow"];
 const a = document.querySelector(".number");
 const b = document.querySelector("#btn1");
 const c = document.querySelector("#btn2");
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
+// simple button event
 
+b.addEventListener("click", function () {
+  const d = getRandomNumber();
+  a.textContent = color[d];
+  document.body.style.backgroundColor = color[d];
+  console.log(d);
+});
 
+function getRandomNumber() {
+  return Math.floor(Math.random() * 4);
+}
 
-b.addEventListener("click",function () {
-      
-      const d = getRandomNumber();
-      a.textContent = color[d];
-      document.body.style.backgroundColor = color[d];
-      console.log(d);
-    
-
-    
-        
-    });
-
-
-    function getRandomNumber() {
-   return Math.floor(Math.random() * 4);
- }
-
-
-
-
-
+// hex button event
 
 c.addEventListener("click", function () {
   let hexColor = "#";
@@ -35,18 +27,13 @@ c.addEventListener("click", function () {
     hexColor += hex[getRandomNumber2()];
   }
 
-
-console.log(hexColor);
+  console.log(hexColor);
 
   a.textContent = hexColor;
-
 
   document.body.style.backgroundColor = hexColor;
 });
 
-
 function getRandomNumber2() {
   return Math.floor(Math.random() * hex.length);
 }
-
-
